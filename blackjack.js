d84f3ef2ec9d5e3c4b1e2a07582a35d6a28adcf2
jusufjulma <jusaleander@hotmail.com>
2018-03-13 04:02:13 +0200

    Added Inspector
    Shuffle is a bit too agressive

--- blackjack.js
+++ blackjack.js
@@ -1,7 +1,7 @@
-var deck = [];
+var deck = []; // global variable for all functions to use
 
 
-function Card(suit, value) {
+function Card(suit, value) { // object constructor
 this.suit = suit;
 this.value = value;
 }
@@ -22,9 +22,102 @@ for (var i = 0; i < suits.length; i++) {
 // triggers for buttons
 $(".start").click(deckCreator);
 $(".shuffle").click(shuffle);
+$(".draw").click(drawCard);
+$(".deal").click(firstDeal);
+
+
 
 function shuffle(){ // this is a massively complicated super-shuffle algorithm
+                    // also totally fucked
 for (var i = 0; i < deck.length; i++) {
   deck[i] = deck[Math.floor(Math.random()*51)]};
   console.log(deck);
 }; // closes shuffle function
+
+// draw the top card and remove it from the deck
+function drawCard(){ // draws first card from array
+console.log(deck[0]);
+deck.splice(0, 1);
+console.log(deck[0]);
+console.log(deck);
+}; // this ends draw function
+
+function firstDeal(){
+var player = []; var dealer = [];
+for (var i = 0; i < 2; i++) {
+// first card goes to player
+var cardhing = deck[0];  // let's select first card
+player.push(cardhing);  // player array gets first card
+console.log("CYKA");    // this did not help debugging
+deck.splice(0, 1);      // getting rid of the dealt card
+
+if (i == 1) { // here we make the second dealer card hidden
+  var cardhing = deck[0];
+  dealer.push(cardhing);  // dealer array gets hidden card
+  console.log("THIS CARD IS HIDDEN!!!");
+  console.log(cardhing);
+  deck.splice(0, 1);      // getting rid of the dealt card
+}else{                         // if ends here, hide dealer's second card
+var cardhing = deck[0]; // let's select first card
+dealer.push(cardhing);  // dealer array gets first card
+console.log("Blyat");   // neither did this
+deck.splice(0, 1);      // getting rid of the dealt card
+} // end of else
+} // end of for
+console.log("Player got these: ");
+console.log(player);
+var x = player;
+blackjackInspector(x);
+console.log("Dealer got these: ");
+console.log(dealer);
+var x = dealer;
+blackjackInspector(x);
+
+
+}; //end of firstDeal
+
+function blackjackInspector(x){ // is this hand a blackjack?
+  console.log("Firing INSPECTOR!!!");
+  var isIt = 0; // is it Blackjack or NOT? This gets the value of given hand
+  for (var i = 0; i < x.length; i++) { // this calculates the value of hand
+    var playerHand = x[i];
+    var playerHand = playerHand.value;
+    console.log(playerHand);
+    isIt = isIt + playerHand;
+  }console.log(isIt);
+  if (isIt >= 3 && isIt <= 5  ) {
+    console.log("ohaC :--D");
+  }else{
+    console.log("jaaha");
+  }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EOF
